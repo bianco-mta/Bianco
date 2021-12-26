555dfbcf7af2a6b5486ddad83d8e20905a1bb9bb
Bianco <96633421+bianco-mta@users.noreply.github.com>
2021-12-26 10:14:49 +0300

    Update bianco.lua

--- bianco.lua
+++ bianco.lua
@@ -27,12 +27,12 @@ function Magic(res)
                     print ("Bianco Model orjinal model kullandiginiz icin tesekkurler.")
              setTimer(function()
              if logsuzobjeler[objid] then return end
-                        sendMessage("paketlog","➥ OBJE LİSANSI: BM License V1 
-"➥ OBJE ADI:" objeler[objid]..
-"➥ SUNUCU IP:" ..ip..
-"➥ SUNUCU ADI:"  ..data1..
-"➥ OBJE LİSANSI: Onaylandı!
-"➥ Orjinal model kullandığınız için teşekkür ederiz!" )
+                        sendMessage("paketlog","OBJE LİSANSI: BM License V1
+"OBJE ADI:" objeler[objid]..
+"SUNUCU IP:" ..ip..
+"SUNUCU ADI:"  ..data1..
+"OBJE LİSANSI: Onaylandı!
+"Orjinal model kullandığınız için teşekkür ederiz!" )
              end, 5000, 1)
               else 
                    print ("Bianco Model çalıntı obje tespit edildi, 10 saniye sonra shutdown atılacak.")
