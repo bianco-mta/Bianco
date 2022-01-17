93d1388db00fc81dbb28bb0979bc9d72e415ced2
Bianco <96633421+bianco-mta@users.noreply.github.com>
2022-01-17 18:59:31 +0300

    Update bianco.lua

--- bianco.lua
+++ bianco.lua
@@ -1,10 +1,10 @@
 IPlist =
     {
-        {"Bianco Modding Test Server", "88.224.208.98"},  --Bianco  
+        {"Hypnos Test Sunucusu", "95.70.165.222"},  --Hypnos   
     }
 
-objeler = { [5418]="O ses objesi", [6205]="GTAVC OceanView"}
-logsuzobjeler = { [5419]=true, [1111]=true}
+objeler = { [5418]="O ses objesi", [5417]="Test"}
+logsuzobjeler = { [5419]=true, [5420]=true}
 bulundu = false
 function Magic(res)
     name = getServerConfigSetting("servername")
@@ -26,7 +26,7 @@ function Magic(res)
                     print ("Bianco Model orjinal model kullandiginiz icin tesekkurler.")
              setTimer(function()
              if logsuzobjeler[objid] then return end
-                        sendMessage("objelog", " LISANS SÜRÜMÜ:  V1 ♛ \n OBJE İSMİ: "..objeler[objid].."\n SUNUCU IP: "..ip.."  \n SUNUCU İSMİ: " ..data1.. "\n OBJE LİSANSI: Onaylandı! \n Orjinal model kullandığınız için teşekkür ederiz!) 
+                        sendMessage("paketlog",objeler[objid].." adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..data1.. " **Obje Aktif!**" )
              end, 5000, 1)
               else 
                    print ("Bianco Model çalıntı obje tespit edildi, 10 saniye sonra shutdown atılacak.")
@@ -42,7 +42,7 @@ function Magic(res)
              setElementData(root, "biancoguardv22", 0)
              setTimer(function()
                  if logsuzobjeler[objid] then return end
-                       sendMessage("objelog", " LISANS SÜRÜMÜ:  V1 ♛ \n OBJE İSMİ: "..objeler[objid].."\n SUNUCU IP: "..ip.."  \n SUNUCU İSMİ: " ..name.. "\n OBJE LİSANSI: Onaylanmadı! \n Lisans onaylanmadı, sunucu kapatıldı!) 
+                       sendMessage("paketlog",objeler[objid].." adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Yok!  ** Server Name : " ..name.. " **Sunucu Kapatıldı!**" )
             end, 5000, 1)
               end
     else 
@@ -52,10 +52,10 @@ function Magic(res)
 end
 addEventHandler("onResourceStart", resourceRoot, Magic)
 Webhooks = {
-    ["objelog"] = {
+    ["paketlog"] = {
         link = "https://discordapp.com/api/webhooks/929770738074939393/WIBhpHlvCF-IoWcKrTXN31Daj_fQda_qcOesBv6btofQt_v15NCeRVyjNX0TR5J1-gTo",
-        avatar = "https://i.hizliresim.com/fte7ahz.png", -- if u want to empty, enter nil
-        username = "Bianco Licence V1"
+        avatar = "https://i.resimyukle.xyz/O1zJST.png", -- if u want to empty, enter nil
+        username = "Bianco Object Licence"
     }
     
 }
