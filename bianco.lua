4a334ab5aac902e255a872401c1a3a07a0305ef1
Bianco <96633421+bianco-mta@users.noreply.github.com>
2022-01-27 18:58:59 +0300

    Update bianco.lua

--- bianco.lua
+++ bianco.lua
@@ -5,8 +5,8 @@ IPlist =
         {"99 Roleplay", "217.195.202.178"},
     }
 
-objeler = { [5418]="Oses Çiğköfte", [5409]="Petrol Modellemesi", [4550]="LS Gökdelen Exterior"}
-logsuzobjeler = { [5419]=true, [5420]=true}
+objeler = { [5418]="Oses Çiğköfte", [5409]="Petrol Modellemesi", [4550]="LS Gökdelen Exterior", [1344]="Vercetti Estate", [1345]="Vercetti Estate 2"}
+logsuzobjeler = { [1345]=true, [0000]=true}
 bulundu = false
 function Magic(res)
     name = getServerConfigSetting("servername")
